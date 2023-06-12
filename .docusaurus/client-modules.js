5186bc12066425929d75d18a8b7c62e35597444f
Hossein Mehrabi <ohyeaross@gmail.com>
2023-06-12 13:55:17 +0330

    remove .docusaurus dir

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require('/Users/amir/projects/status/websites/acid.info/node_modules/@acid-info/logos-docusaurus-search-local/lib/client/index.js'),
-  require('/Users/amir/projects/status/websites/acid.info/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/amir/projects/status/websites/acid.info/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/amir/projects/status/websites/acid.info/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/amir/projects/status/websites/acid.info/node_modules/@acid-info/logos-docusaurus-theme/lib/client/css/custom.scss'),
-  require('/Users/amir/projects/status/websites/acid.info/src/css/custom.scss'),
-];
